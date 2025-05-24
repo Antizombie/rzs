b1503c7e3d88c7c43044c02160a4eddd33ecec39
Мяско :3 <3177865@mail.ru>
2025-05-24 19:53:43 +0300

    Create

---
+++ gamemode/shared.lua
@@ -0,0 +1,9 @@
+FProfiler.start()
+GM.Name = "Russian Zombie Survival"
+GM.Author = "Мяско :3"
+GM.Email = "s.semenov@stasmesnik.ru"
+GM.Website = "https://www.stasmesnik.ru/"
+
+function GM:Initialize()
+	-- Do stuff
+end
