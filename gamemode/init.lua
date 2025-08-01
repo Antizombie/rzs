82f00d057524a62e6f216a5f712136bb5afedeae
Станислав <3177865@mail.ru>
2025-08-01 08:14:06 +0000

    add sh_global.lua and sv_init.lua

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -1,4 +1,8 @@
+--Файл инициализации, тут только AddCSLuaFile и include (пока), сюда нужно пихать то, что врятли поменяеться.
 AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "sh_global.lua" )
 AddCSLuaFile( "shared.lua" )
 
+include( "sh_global.lua" )
 include( "shared.lua" )
+include( "sv_init.lua" )
--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -1,4 +1,4 @@
-FProfiler.start()
+--FProfiler.start()
 GM.Name = "Russian Zombie Survival"
 GM.Author = "Мяско :3"
 GM.Email = "s.semenov@stasmesnik.ru"
@@ -6,4 +6,9 @@ GM.Website = "https://www.stasmesnik.ru/"
 
 function GM:Initialize()
 	-- Do stuff
+end
+
+function GM:CreateTeams()
+	team.SetUp(TEAM_ZOMBIE, "Зомби", Color(0, 255, 0, 255))
+	team.SetUp(TEAM_SURVIVORS, "Люди", Color(0, 160, 255, 255))
 end
